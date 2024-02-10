006012b08463cba8c0ed26986bfd9eb106f8c896
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-02-10 15:54:19 +0600

    Add files via upload

---
+++ 02d/01.php
@@ -0,0 +1,10 @@
+<?php
+
+$s=9;
+$f=0;
+
+echo"Happy ending only happens in Movies\n";
+print $s+$f;
+echo"\n";
+echo 'how ','can ','use ','this ','in ','backend ','tell me ','how!';
+?>
---
+++ 02d/02.php
@@ -0,0 +1,3 @@
+<?php
+
+echo"What did you learn kid?\n Did you ever practice web tech today?\n aren't you stuct with your fucking emotion";
