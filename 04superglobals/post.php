c2846252bc2ef189e2167fa3f6c17eaa057e4417
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-02-13 23:21:43 +0600

    Add files via upload

---
+++ 04superglobals/post.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <fieldset>
+            <legend>Post section</legend>
+            <?php echo $_POST["status"]; ?>
+        </fieldset>
+    </body>
+</html>
---
+++ 04superglobals/superglobal.php
@@ -0,0 +1,6 @@
+
+
+<h1>Welcome <?php echo $_GET["name"]; ?> </h1>
+
+
+<p>YOUR EMAIL IS : <?php echo $_GET["email"] ?></p>
