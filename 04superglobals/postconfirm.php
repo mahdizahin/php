8debb074072bf02786cae59a258f121594763ff2
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-02-13 23:38:17 +0600

    Add files via upload

---
+++ 04superglobals/postconfirm.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <div>
+            <p> User add a post </p>
+            <fieldset>
+               
+                <?php echo $_POST["text"]; ?>
+            </fieldset>
+        </div>
+    </body>
+</html>
