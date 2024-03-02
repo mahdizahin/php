ade94df3f951d896c7b8f309bcd201b4551191de
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-03-03 02:11:38 +0600

    Add files via upload

---
+++ 05Loop/001foreach.php
@@ -0,0 +1,9 @@
+<?php
+
+$gateOfBashundhara = array("C Block 8 number Pocket gate","Bashundhara Gate,beside Jamuna Future park","Applo Gate","300 feet Gate");
+
+foreach ($gateOfBashundhara as $gate){
+    echo "Gate : {$gate}<br>" ;
+}
+
+?>
---
+++ 05Loop/002foreachindexSee.php
@@ -0,0 +1,10 @@
+<?php
+
+$gateOfBashundhara = array("C Block 8 number Pocket gate","Bashundhara Gate,beside Jamuna Future park","Applo Gate","300 feet Gate");
+
+foreach ($gateOfBashundhara as $index=>$gate){
+    $index=$index+1;
+    echo "Gate No {$index}: {$gate}<br>" ;
+}
+
+?>
