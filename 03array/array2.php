9f2107b8803386cfff55a0319aad5362028a5ec2
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-03-03 01:11:02 +0600

    Add files via upload

---
+++ 03array/array2.php
@@ -0,0 +1,9 @@
+<?php
+
+$userName = array("malik"=>5,"kabir"=>59);
+
+print_r($userName);
+echo "</br>";
+print $userName["malik"];
+
+?>
---
+++ 03array/associative_array.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "run";
+$arr = array("Dune 2","Exp 4 ","fint");
+
+for ($i=0; $i<3; $i++){
+echo "</br> Index {$i}'s Value is  {$arr[$i]} ";
+}
+?>
