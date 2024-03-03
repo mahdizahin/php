c4c542157dc73c2cde5b2fa56ec34b569498caf2
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2024-03-03 09:53:36 +0600

    Create 005foreach.php

---
+++ 05Loop/005foreach.php
@@ -0,0 +1,20 @@
+<?php
+
+$player = array( 
+                array("VAN DJK","NED","LIV"),
+                array("MILITAO","BRA","RM")
+  
+              );
+
+foreach($player as $subarray => $arraysubstance){
+
+foreach($arraysubstance as $innerArraySubstance){
+echo $innerArraySubstance;
+
+}
+  
+
+
+}
+
+?>
